03c82b53fac98fe94017b0a6e74472f13b84fb6a
billy <wewill147@gmail.com>
2017-12-21 16:46:02 -0600

    finds mouse pointer on first try, must be reset each time

--- arm2dof.py
+++ arm2dof.py
@@ -17,8 +17,8 @@ display = pygame.display.set_mode((width, height))
 pygame.display.set_caption("2 DOF Robot Arm")
 fpsClock = pygame.time.Clock()
 
-l1 = ArmSegment(150.)
-l2 = ArmSegment(300.)
+l1 = ArmSegment(300.)
+l2 = ArmSegment(150.)
 
 l1.arm.fill(arm_color)
 l2.arm.fill(arm_color)
@@ -40,54 +40,31 @@ def transform_lines(rect, base, arm_segment):
 def normalize_points(point):
     return point[0] - 400, 400 - point[1]
 
+def fix(arg):
+    if arg > 1:
+        return 1
+    if arg < -1:
+        return -1
+    return arg
 
 def get_angles(point):
-    a = l2.length
-    b = l1.length
-    angle_d = np.arctan2(point[1], point[0])
-    c = math.sqrt(point[0]**2 + point[1]**2)
-    angle_a = np.arccos((b**2 + c**2 - a**2) / (2*b*c))
-    angle1 = angle_d - angle_a
-    angle2 = np.arcsin((np.sin(angle_a) * c)/a)
-    return angle1, angle2 + angle1
-
-
-def get_angles1(point):
-    a, b = l2.length, l1.length
-    xf, yf = point
-    xy2 = xf**2 + yf**2
-    angle_d = np.arctan2(-yf/math.sqrt(xy2), xf/math.sqrt(xy2))
-    c = math.sqrt(xy2)
-    # angle_a = np.arccos((b**2 + c**2 - a**2) / (2*b*c))
-    th = np.arccos(-1 * (xy2 + b**2 - a**2) / math.sqrt(((-2 * b * xf)**2) + ((-2 * b * yf)**2)))
-    angle_a = angle_d - th
-    angle1 = angle_d + th  # angle_a
-    angle2 = np.arcsin((np.sin(angle_a) * c)/a)
-    return angle1, angle2
-
-
-def get_angles2(point):
-    a, b = l2.length, l1.length
-    x, y = point
-    xy2 = x**2 + y**2
-
-    angle_d = np.arctan2(-y / math.sqrt(xy2), x / math.sqrt(xy2))
-    th2 = np.arctan2(
-            math.sqrt(1-((xy2 - b**2 - a**2)/(2 * b * a))**2),
-            (xy2 - b**2 - a**2)/(2 * b * a))
-    th1 = np.arctan2(y, x) - angle_d
-    return th1, th2
-
-
-def get_angles3(point):
-    a = l2.length
-    b = l1.length
-    angle_d = np.arctan2(point[1], point[0])
-    c = math.sqrt(point[0]**2 + point[1]**2)
-    angle_a = np.arccos((b**2 + c**2 - a**2) / (2*b*c))
-    angle1 = angle_d - angle_a
-    angle2 = np.arcsin((np.sin(angle_a) * c)/a)
-    return angle1, angle2
+    A, Z = point
+    angle_d = np.arctan2(Z, A)
+    print(np.degrees(angle_d))
+    top = (l1.length**2 + A**2 + Z**2 - l2.length**2)
+    bottom = 2*l1.length * math.sqrt(A**2 + Z**2)
+    first_arm = top/bottom
+    first_arm = fix(first_arm)
+    second_arm = (l1.length**2 + l2.length**2 - A**2 - Z**2)/(2 * l1.length * l2.length)
+    second_arm = fix(second_arm)
+    angle_1, angle_2 = np.arccos([first_arm, second_arm])
+    print(np.degrees(angle_1))
+    print(np.degrees(angle_2))
+    angle_1 = angle_1 + angle_d
+    angle_2 = (angle_2 + angle_1) - math.pi
+    print(np.degrees(angle_1))
+    print(np.degrees(angle_2))
+    return angle_1, angle_2
 
 
 def update_frame():
@@ -103,13 +80,13 @@ def update_frame():
                           l2.scale * np.sin(l2.rotation)]) * -1 + origin[1]
 
     joints = [(int(x), int(y)) for x, y in zip(joints_x, joints_y)]
-
+    
     # rotate arm lines
     line_l1 = pygame.transform.rotozoom(l1.arm,
                                         np.degrees(l1.rotation), 1)
     line_l2 = pygame.transform.rotozoom(l2.arm,
                                         np.degrees(l2.rotation), 1)
-
+    
     # translate arm lines
     l1_rect = line_l1.get_rect()
     transform_lines(l1_rect, joints[0], l1)
@@ -134,33 +111,33 @@ while 1:
 
     l1.rotation = 0
     l2.rotation = 0
-    print 'Robot ready for mission.'
+    print('Robot ready for mission.')
 
     reset = False
     while not reset:
         update_frame()
         for event in pygame.event.get():
             if event.type == locals.QUIT:
-                print 'ALL YOUR BASE ARE BELONG TO US.'
+                print('ALL YOUR BASE ARE BELONG TO US.')
                 pygame.quit()
                 sys.exit()
             if event.type == locals.KEYDOWN:
-                if pygame.key.get_pressed()[32]:
-                    print 'Resetting Arm.'
+                if pygame.key.get_pressed()[32]: # if spacebar is pressed
+                    print('Resetting Arm.')
                     reset = True
                     break
             if event.type == locals.MOUSEBUTTONUP:
                 target = normalize_points(mouse.get_pos())
-                print target
+                print(target)
                 angle_l1, angle_l2 = get_angles(target)
 
-                print ''
-                print 'CHANGE >>\nAngle 1: {: f}\nAngle 2: {: f}\n'.format(np.degrees(angle_l1), np.degrees(angle_l2))
+                print('')
+                print('CHANGE >>\nAngle 1: {: f}\nAngle 2: {: f}\n'.format(np.degrees(angle_l1), np.degrees(angle_l2)))
 
                 l1_arm, l1_rect = l1.rotate(angle_l1)
                 l2_arm, l2_rect = l2.rotate(angle_l2)
 
-                print 'CURRENT >>\nAngle 1: {: f}\nAngle 2: {: f}\n'.format(np.degrees(l1.rotation), np.degrees(l2.rotation))
+                print('CURRENT >>\nAngle 1: {: f}\nAngle 2: {: f}\n'.format(np.degrees(l1.rotation), np.degrees(l2.rotation)))
 
                 # rotate_l1, rotate_l2 = True, True
                 # while rotate_l1 or rotate_l2:  # l1.rotation < angle_l1 and l2.rotation < angle_l2:
